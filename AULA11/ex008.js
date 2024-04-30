68456dce46a1d5a68c23a7a30b1b091d2308096b
Gabriel Bento <100233749+gabriel96651@users.noreply.github.com>
2024-04-29 22:10:44 -0300

    submit

---
+++ AULA11/ex008.js
@@ -0,0 +1,5 @@
+var vel =60.5
+console.log(`a velocidade do seu carro e de ${vel} km/h`)
+if (vel > 60) {
+    console.log(`voce ultrapassou o limite de velocidade permitida(60km/h).MULTADO`)}
+console.log(`dirija sempre usando o cinto de seguranca!`)
