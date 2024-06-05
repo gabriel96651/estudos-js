135e354f10a6f367b3b664d60c725860280d4f54
Gabriel Bento <100233749+gabriel96651@users.noreply.github.com>
2024-06-05 19:35:09 -0300

    aula 13 e 14

---
+++ AULA013/ex014.js
@@ -0,0 +1,38 @@
+var agora = new Date()
+var diaSem = agora.getDay()
+/*
+    0 = domingo
+    1 = segunda
+    2 = terca
+    3 = quata
+    4 = quinta
+    5 = sexta
+    6 = sabado
+*/
+
+switch(diaSem) {
+    case 0:
+        console.log('domingo')
+        break
+    case 1:
+        console.log('segunda')
+        break
+    case 2:
+        console.log('terca')
+        break
+    case 3:
+        console.log('quarta')
+        break
+    case 4:
+        console.log('quinta')
+        break
+    case 5:
+        console.log('sexta')
+        break
+    case 6:
+        console.log('sabado')
+        break
+    default:
+        console.log('[ERRO] dia invalido')
+        break
+}
